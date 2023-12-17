894362b51b7c56094e0f19c1c186134f9f829c31
XFactHD <xfacthd@gmx.de>
2023-12-17 04:01:50 +0100

    Accommodate pack.mcmeta, dependency and pack grouping changes in NeoForge

--- neoforge/src/main/java/xfacthd/atlasviewer/platform/NeoForgePlatformHelper.java
+++ neoforge/src/main/java/xfacthd/atlasviewer/platform/NeoForgePlatformHelper.java
@@ -5,17 +5,14 @@ import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.renderer.texture.atlas.SpriteSource;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.PackResources;
-import net.minecraft.server.packs.PackType;
 import net.neoforged.fml.ModLoader;
 import net.neoforged.fml.loading.FMLLoader;
 import net.neoforged.fml.loading.FMLPaths;
-import net.neoforged.neoforge.resource.DelegatingPackResources;
 import xfacthd.atlasviewer.client.api.RegisterSpriteSourceDetailsEvent;
 import xfacthd.atlasviewer.client.util.SpriteSourceManager;
 import xfacthd.atlasviewer.platform.services.IPlatformHelper;
 
 import java.nio.file.Path;
-import java.util.Objects;
 import java.util.stream.Stream;
 
 public final class NeoForgePlatformHelper implements IPlatformHelper
@@ -58,14 +55,6 @@ public final class NeoForgePlatformHelper implements IPlatformHelper
     @Override
     public Stream<String> getPackIDs(PackResources pack, ResourceLocation loc)
     {
-        if (pack instanceof DelegatingPackResources delPack)
-        {
-            return Objects.requireNonNull(delPack.getChildren())
-                    .stream()
-                    .filter(d -> d.getResource(PackType.CLIENT_RESOURCES, loc) != null)
-                    .map(PackResources::packId)
-                    .map(id -> "\"" + pack.packId() + "\" -> \"" + (id.isEmpty() ? " " : id) + "\"");
-        }
         return Stream.of(pack.packId());
     }
 
